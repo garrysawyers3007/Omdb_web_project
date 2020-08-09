a33037ddee10e5896e4c51a10b95010f97da4078
Gauransh <gauransh30@gmail.com>
2020-08-10 00:11:40 +0530

    Deployment

---
+++ omdb-backend/public/precache-manifest.b95082ab14ed4f2cbac8e2bf83d1cf95.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e71abb1994308a68ddaa8623567c5ce",
+    "url": "/index.html"
+  },
+  {
+    "revision": "79abd535e1fa91d9c46e",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "6267f747ee7b563ae7db",
+    "url": "/static/css/main.d180690c.chunk.css"
+  },
+  {
+    "revision": "79abd535e1fa91d9c46e",
+    "url": "/static/js/2.fb38bfe7.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.fb38bfe7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6267f747ee7b563ae7db",
+    "url": "/static/js/main.66ea98dc.chunk.js"
+  },
+  {
+    "revision": "87f9cc7c2d39d40ed91e",
+    "url": "/static/js/runtime-main.1d58dd66.js"
+  }
+]);
